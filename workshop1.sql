a8a34d2671c115ba8009b951939efbe149db75f6
Chako <ryland0000@gmail.com>
2023-09-01 22:13:56 -0700

    orders alter table

--- workshop1.sql
+++ workshop1.sql
@@ -107,9 +107,23 @@ CREATE TABLE us_states(
     abbreviation CHARACTER(2) NOT NULL UNIQUE,
     PRIMARY KEY(id)
 );
+
+
+
+
 ---
 --- Add foreign key constraints
----
+---/* add alter table to add foreign key constraints */
+
+ALTER TABLE orders
+ADD CONSTRAINT fk_orders_customers
+FOREIGN KEY (customer_id)
+REFERENCES customers;
+
+ALTER TABLE orders
+ADD CONSTRAINT fk_orders_employees
+FOREIGN KEY (employee_id)
+REFERENCES employees;
 
 -- PRODUCTS
 
